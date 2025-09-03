6e02fab94957adac8f2cf4f5b1dfb7528ce4373c
24550261 <L24550261@chihuahua2.tecnm.mx>
2025-09-02 18:00:18 -0600

    Add files via upload

---
+++ EVA1_12_ARREGLOS_MULTI/src/eva1_12_arreglos_multi/EVA1_12_ARREGLOS_MULTI.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_12_arreglos_multi;
+
+/**
+ *
+ * @author oujeffu
+ */
+public class EVA1_12_ARREGLOS_MULTI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [][] arreglos= new int [5][3];
+        arreglos[0][0]=100;
+        System.out.println(arreglos.length);
+        System.out.println(arreglos[0].length);
+        System.out.println(arreglos[1].length);
+        System.out.println(arreglos[2].length);
+        System.out.println(arreglos[3].length);
+        System.out.println(arreglos[4].length);
+        
+        
+        int [][] datos= new int [3][];
+        
+        datos[0]=new int [10];
+        datos[1]=new int [5];
+        datos[2]=new int [1];
+        
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                datos[i][j]=(int)(Math.random()*100);
+            }
+        }
+        
+        
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.print("["+datos[i][j]+"]");
+                System.out.print("");
+            }
+                            System.out.println("");
+        }
+                
+    }
+    
+}
